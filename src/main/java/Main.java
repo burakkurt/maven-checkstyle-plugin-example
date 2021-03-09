01b91e6679d1206a6b0eee637294902794773adc
Burak Kurt <burak.kurt@trendyol.com>
2021-03-09 13:14:46 +0300

    fix errors for new rules

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,12 +15,17 @@ public class Main {
 
     public static final String INVALID_CONSTANT_NAME = "Constant names should be uppercase";
     public static final String WRONG_MODIFIER_ORDER = "static should be before final";
+    public static final String WORD = "something";
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+    public static final int NINE = 9;
 
     public static void main(final String[] args) {
         oneLineCannotExceedOneHundredTwentyCharacters();
         thereMustBeOneWhiteSpaceAfterIfAndClosingParanthesesCondition();
         avoidUsingStarImport();
         avoidMoreThanThreeReturnStatement();
+        avoidMoreThanThreeOperatorsForBoolean();
     }
 
     private static void oneLineCannotExceedOneHundredTwentyCharacters() {
@@ -41,14 +46,33 @@ public class Main {
     }
 
     private static int avoidMoreThanThreeReturnStatement() {
-        final String word = "something";
         final int defaultValue = 3;
 
-        if ("somethingg".equals(word)) {
+        if ("somethingg".equals(WORD)) {
             return 0;
         }
 
         return defaultValue;
+    }
+
+    private static boolean avoidMoreThanThreeOperatorsForBoolean() {
+        if (isFirstAndLastCharactersAreAsExpected("s", "g") && WORD.length() == NINE
+                && WORD.charAt(FIVE) == 'o' && WORD.substring(1, FOUR).equals("asd")) {
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isFirstAndLastCharactersAreAsExpected(final String firstCharacter,
+                                                                 final String lastCharacter) {
+
+        if (WORD.startsWith(firstCharacter) && WORD.endsWith(lastCharacter)) {
+            return true;
+        }
+
+        return false;
     }
 
 }
